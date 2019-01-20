ab00d6836a60d4e74f04dcd195c87d464a4b88c5
kevinta893 <kevinta893@gmail.com>
2019-01-20 16:23:57 +0900

    Refactor install script to use URL arguments

--- travis-build/install-unity.sh
+++ travis-build/install-unity.sh
@@ -2,39 +2,38 @@
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
 # of available packages, where PLATFORM is `osx` or `win`
-
+UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
 UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/0a46ddfcfad4/MacEditorInstaller/Unity-2018.2.12f1.pkg"
 UNITY_WINDOWS_TARGET_PACKAGE_URL="http://download.unity3d.com/download_unity/0a46ddfcfad4/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2018.2.12f1.pkg"
 
 
 download() {
-	
-	FILE=$1
-	URL="$BASE_URL/$FILE"
+
+	URL=$1
 
 	#download package if it does not already exist in cache
-	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$FILE"` ] ; then
+	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$URL"` ] ; then
 		echo "$FILE does not exist. Downloading from $URL: "
-		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$FILE"` "$URL"
+		mkdir -p "$UNITY_DOWNLOAD_CACHE"
+		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$URL"` "$URL"
 	else
 		echo "$FILE Exists. Skipping download."
 	fi
 }
 
 install() {
-	PACKAGE=$1
-	download "$PACKAGE"
-	
-	echo "Installing "`basename "$PACKAGE"`
-	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE"` -target /
+	PACKAGE_URL=$1
+	download "$PACKAGE_URL"
+
+	echo "Installing $PACKAGE_NAME"
+	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE_URL"` -target /
 }
 
 
 
 echo "Contents of Unity Download Cache:"
-ls "$UNITY_DOWNLOAD_CACHE"
+ls $UNITY_DOWNLOAD_CACHE
 
 echo "Installing Unity..."
 install "$UNITY_OSX_PACKAGE"
 install "$UNITY_WINDOWS_TARGET_PACKAGE"
-
