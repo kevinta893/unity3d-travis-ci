7b0689a64d1f48114a3518e8402d386eff48751f
kevinta893 <kevinta893@gmail.com>
2019-02-03 11:31:32 +0900

    Add Unit testing scripts using Unity TestRunner (not automated CI yet)

---
+++ UnityExample/Assets/Tests/TestScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestScript
+{
+
+    [Test]
+    public void TestScript_SimplePasses()
+    {
+        // Use the Assert class to test conditions.
+        var expected = true;
+        var actual = true;
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void TestScript_ThrowException()
+    {
+        var gameObject = new GameObject("TestObject_TestScript_ThrowException()");
+
+        Assert.Throws<MissingComponentException>(
+            () => gameObject.GetComponent<Rigidbody>().velocity = Vector3.one
+        );
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator TestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
